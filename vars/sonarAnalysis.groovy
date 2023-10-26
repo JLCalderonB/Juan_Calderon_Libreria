ca5b4eadc463450b76b10ccc6580357b6b389b2c
Juan Luis Calderón <88913850+JLCalderonB@users.noreply.github.com>
2023-10-26 08:27:08 -0300

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,8 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(Boolean bool_1="true", Boolean bool_2="false") {
   
     def Result = sh 'echo "ejecuciooooooooooooooooooon"'
+    echo "Booleano_1 : ${bool_1}."
+    echo "Booleano_2 : ${bool_2}."
     println Result
   
 }
