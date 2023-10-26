e8b66f54c9612dff1ef0278994ee3a8d33c0f7f8
Juan Luis Calderón <88913850+JLCalderonB@users.noreply.github.com>
2023-10-26 12:03:23 -0300

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -3,8 +3,8 @@
 def call(boolean bool_1, boolean bool_2) {
     def scannerHome = tool 'SonarScanner';
     def PROJECT_NAME = "practica_1_2023_JUAN_CALDERON";
-    def SONAR_AUTH_TOKEN = 'SQJenkinsToken';
-    def Result1 = sh 'echo "ejecuciooooooooooooooooooon"'
+    def SONAR_AUTH_TOKEN = "SQJenkinsToken";
+    def Result1 = sh 'echo "Token "+SQJenkinsToken'
     echo "Booleano_1 : ${bool_1}."
     echo "Booleano_2 : ${bool_2}."
     println Result1
