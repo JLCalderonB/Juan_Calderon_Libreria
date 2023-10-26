af42f29aff791d81c8a43be5898bd7099b6b2f26
Juan Luis Calderón <88913850+JLCalderonB@users.noreply.github.com>
2023-10-26 13:12:55 -0300

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -4,13 +4,14 @@ def call(tokensq, boolean bool_1, boolean bool_2) {
     def scannerHome = tool 'SonarScanner';
     def PROJECT_NAME = "practica_1_2023_JUAN_CALDERON";
     def SONAR_AUTH_TOKEN = tokensq;
+    def SONAR_HOST_URL = 'http://localhost:9000';
     def Result1 = sh 'echo "Token : "+${SONAR_AUTH_TOKEN}'
     echo "token : ${tokensq}."
     echo "token : ${SONAR_AUTH_TOKEN}."
     echo "Booleano_1 : ${bool_1}."
     echo "Booleano_2 : ${bool_2}."
     println Result1
-    def Result2 = sh (script: "${scannerHome}/bin/sonar-scanner -Dsonar.login=${tokensq} -Dsonar.projectKey=${PROJECT_NAME} ", returnStdout: true)
+    def Result2 = sh (script: "${scannerHome}/bin/sonar-scanner -Dsonar.login=${tokensq} -Dsonar.projectKey=${PROJECT_NAME} -Dsonar.host.url=${SONAR_HOST_URL}", returnStdout: true)
     println Result2
   
 }
