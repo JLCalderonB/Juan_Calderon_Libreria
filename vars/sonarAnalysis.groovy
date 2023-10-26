173beaaa7a0264d830c3ffbcd12be59ec94c778c
Juan Luis Calderón <88913850+JLCalderonB@users.noreply.github.com>
2023-10-26 00:21:11 -0300

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,4 +1,16 @@
 @sonarAnalysis
+
 def call() {
-  echo "entre a la librería"
+  def scannerHome = tool 'SonarScanner';
+  def PROJECT_NAME = "practica_1_2023_JUAN_CALDERON";
+  println params
+  withSonarQubeEnv(installationName: 'sq1', credentialsId: 'SQJenkinsToken') { 
+    def Result = sh (script: "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${PROJECT_NAME} ", returnStdout: true)
+    println Result
+  
+  }
+  sleep(10)
+  timeout(time: 5, unit: 'MINUTES') {
+    waitForQualityGate abortPipeline: true
+  }
 }
