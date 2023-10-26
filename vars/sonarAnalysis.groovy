dd58d67e6ad7c1dbd8b25e4a433a7bab808fdb37
Juan Luis Calderón <88913850+JLCalderonB@users.noreply.github.com>
2023-10-26 11:50:31 -0300

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -5,7 +5,8 @@ def call(boolean bool_1, boolean bool_2) {
     def Result = sh 'echo "ejecuciooooooooooooooooooon"'
     echo "Booleano_1 : ${bool_1}."
     echo "Booleano_2 : ${bool_2}."
-    def Result = sh (script: "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${PROJECT_NAME} ", returnStdout: true)
-    println Result
+    println Result1
+    def Result2 = sh (script: "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${PROJECT_NAME} ", returnStdout: true)
+    println Result2
   
 }
