483e9c362f962c85924a044e580e6b8e03395b82
Juan Luis Calderón <88913850+JLCalderonB@users.noreply.github.com>
2023-10-27 18:59:43 -0300

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -36,7 +36,7 @@ def call(tokensq, boolean bool_1, boolean abortPipeline,  git_branch) {
                         waitForQualityGate abortPipeline: true
                         error("Aborto de Pipeline - gatillado por error en QualityGate y por rama que comienza por 'hotfix%'")
                     } else {
-                        echo "Si falla QualityGate CONTINUARÁ el Pipeline"
+                        echo "Si falla QualityGate CONTINUARÁ el Pipeline, rama actual *"+git_branch+"* <> 'master' y no comienza por 'hotfix'"
                         waitForQualityGate abortPipeline: false
                     }
                 } 
