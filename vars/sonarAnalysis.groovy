cfd67fbe2d8a5643fee066fcbc45d7c63a19eacb
Juan Luis Calderón <88913850+JLCalderonB@users.noreply.github.com>
2023-10-27 14:49:23 -0300

    Update sonarAnalysis.groovy

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -5,12 +5,12 @@ def call(tokensq, boolean bool_1, boolean abortPipeline) {
     def PROJECT_NAME = "practica_1_2023_JUAN_CALDERON";
     def SONAR_AUTH_TOKEN = tokensq;
     def SONAR_HOST_URL = 'http://localhost:9000';
-    
+    /*
     echo "tokensq : ${tokensq}."
     echo "SONAR_AUTH_TOKEN : ${SONAR_AUTH_TOKEN}."
     echo "Booleano_1 : ${bool_1}."
     echo "Booleano_2 : ${abortPipeline}."
-    
+    */
     if (abortPipeline) {
         currentBuild.result = 'ABORTED'
         error("Aborto de Pipeline - gatillado por parámetro ingresado abortPipeline")
@@ -19,16 +19,14 @@ def call(tokensq, boolean bool_1, boolean abortPipeline) {
         def Result = sh (script: "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${PROJECT_NAME} -Dsonar.login=${tokensq}", returnStdout: true)
         println Result
         }
-       
-            timeout(time: 1, unit: 'SECONDS') {
-                def qgResult = waitForQualityGate()
-                println qgResult.status
-                echo "qgResult.status: "+qgResult.status
-               /* if (qgResult.status!= 'OK'){ 
-                       if (bool_1){
-                          error "Pipeline aborted due to quality gate failure: ${qg.status}"
-                        }
-                } */ 
+        timeout(time: 5, unit: 'MINUTES') {
+            if (bool_1) {
+                echo "Si falla QualityGate ABORTARÁ el Pipeline"
+                waitForQualityGate abortPipeline: true
+            } else {
+                 echo "Si falla QualityGate CONTINUARÁ el Pipeline"
+                waitForQualityGate abortPipeline: false
+            }
         }     
     }
 }
